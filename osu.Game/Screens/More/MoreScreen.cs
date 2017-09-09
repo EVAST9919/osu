8f65be5a102319c8de933ac4ba7014ac08502d22
EVAST9919 <megaman9919@gmail.com>
2017-09-09 14:20:55 +0300

    Add buttons to the main screen

--- osu.Game/Screens/More/MoreScreen.cs
+++ osu.Game/Screens/More/MoreScreen.cs
@@ -1,15 +1,63 @@
 ﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Game.Graphics.Containers;
+using osu.Game.Graphics.Sprites;
 
 namespace osu.Game.Screens.More
 {
     public class MoreScreen : OsuScreen
     {
+        private ScreenButton visualizer;
+
+        public MoreScreen()
+        {
+            Children = new Drawable[]
+            {
+                new FillFlowContainer
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    AutoSizeAxes = Axes.Both,
+                    Direction = FillDirection.Horizontal,
+                    Children = new Drawable[]
+                    {
+                        visualizer = new ScreenButton(@"Visualizer"),
+                    }
+                }
+            };
+        }
+
+        private class ScreenButton : OsuClickableContainer
+        {
+            private readonly Box background;
+            private readonly OsuSpriteText text;
+
+            public ScreenButton(string title)
+            {
+                Size = new Vector2(100);
+                Children = new Drawable[]
+                {
+                    background = new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Colour = Color4.White,
+                    },
+                    text = new OsuSpriteText
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Text = title,
+                        TextSize = 20,
+                        Colour = Color4.Black,
+                    }
+                };
+            }
+        }
     }
 }
