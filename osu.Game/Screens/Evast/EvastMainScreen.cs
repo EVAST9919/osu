f3391f946e1ec8de7f5b50552be90ba4d2768fcd
Andrei Zavatski <megaman9919@gmail.com>
2019-09-19 04:46:29 +0300

    remove export screen

--- osu.Game/Screens/Evast/EvastMainScreen.cs
+++ osu.Game/Screens/Evast/EvastMainScreen.cs
@@ -1,6 +1,5 @@
 ﻿using osu.Game.Screens.Evast.NumbersGame;
 using osu.Framework.Screens;
-using osu.Game.Screens.Evast.ExportArea;
 using osu.Game.Screens.Evast.Particles;
 using osu.Game.Screens.Evast.ApiHelper;
 
@@ -14,7 +13,6 @@ namespace osu.Game.Screens.Evast
             {
                 new Button("2048", () => this.Push(new NumbersGameScreen())),
                 new Button("Pixel Games", () => this.Push(new PixelsSelectableScreen())),
-                new Button("Export Area", () => this.Push(new ExportAreaScreen())),
                 new Button("Particles", () => this.Push(new SpaceParticlesScreen())),
                 new Button("Music Visualizers", () => this.Push(new MusicVisualizersSelectableScreen())),
                 new Button("APIv2 Helper", () => this.Push(new ApiV2HelperScreen())),
