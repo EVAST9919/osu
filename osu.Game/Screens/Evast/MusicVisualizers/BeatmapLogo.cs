c764237ec7595a0f8c53de318fcd4ff8b4c14c81
Andrei Zavatski <megaman9919@gmail.com>
2020-03-13 11:08:04 +0300

    Implement BeatmapLogo component

---
+++ osu.Game/Screens/Evast/MusicVisualizers/BeatmapLogo.cs
@@ -0,0 +1,57 @@
+﻿using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Screens.Evast.UserInterface;
+using osuTK;
+
+namespace osu.Game.Screens.Evast.MusicVisualizers
+{
+    public class BeatmapLogo : CompositeDrawable
+    {
+        private const int circle_size = 400;
+
+        public BeatmapLogo()
+        {
+            Origin = Anchor.Centre;
+            AutoSizeAxes = Axes.Both;
+
+            AddRangeInternal(new Drawable[]
+            {
+                new MusicCircularVisualizer
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    DegreeValue = 120,
+                    BarWidth = 2,
+                    BarsAmount = 80,
+                    CircleSize = circle_size,
+                },
+                new MusicCircularVisualizer
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    DegreeValue = 120,
+                    Rotation = 120,
+                    BarWidth = 2,
+                    BarsAmount = 80,
+                    CircleSize = circle_size,
+                },
+                new MusicCircularVisualizer
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    DegreeValue = 120,
+                    Rotation = 240,
+                    BarWidth = 2,
+                    BarsAmount = 80,
+                    CircleSize = circle_size,
+                },
+                new BeatmapCircleCard
+                {
+                    Size = new Vector2(circle_size),
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                }
+            });
+        }
+    }
+}
--- osu.Game/Screens/Evast/MusicVisualizers/BeatmapPreviewScreen.cs
+++ osu.Game/Screens/Evast/MusicVisualizers/BeatmapPreviewScreen.cs
@@ -1,15 +1,11 @@
 ﻿using osu.Framework.Graphics;
 using osu.Game.Graphics.Containers;
 using osu.Game.Screens.Evast.Particles;
-using osu.Game.Screens.Evast.UserInterface;
-using osuTK;
 
 namespace osu.Game.Screens.Evast.MusicVisualizers
 {
     public class BeatmapPreviewScreen : EvastScreen
     {
-        private const int circle_size = 400;
-
         public BeatmapPreviewScreen()
         {
             AddRangeInternal(new Drawable[]
@@ -18,43 +14,9 @@ namespace osu.Game.Screens.Evast.MusicVisualizers
                 new ParallaxContainer
                 {
                     ParallaxAmount = -0.0025f,
-                    Children = new Drawable[]
+                    Child = new BeatmapLogo
                     {
-                        new MusicCircularVisualizer
-                        {
-                            Anchor = Anchor.Centre,
-                            Origin = Anchor.Centre,
-                            DegreeValue = 120,
-                            BarWidth = 2,
-                            BarsAmount = 40,
-                            CircleSize = circle_size,
-                        },
-                        new MusicCircularVisualizer
-                        {
-                            Anchor = Anchor.Centre,
-                            Origin = Anchor.Centre,
-                            DegreeValue = 120,
-                            Rotation = 120,
-                            BarWidth = 2,
-                            BarsAmount = 40,
-                            CircleSize = circle_size,
-                        },
-                        new MusicCircularVisualizer
-                        {
-                            Anchor = Anchor.Centre,
-                            Origin = Anchor.Centre,
-                            DegreeValue = 120,
-                            Rotation = 240,
-                            BarWidth = 2,
-                            BarsAmount = 40,
-                            CircleSize = circle_size,
-                        },
-                        new BeatmapCircleCard
-                        {
-                            Size = new Vector2(circle_size),
-                            Anchor = Anchor.Centre,
-                            Origin = Anchor.Centre,
-                        }
+                        Anchor = Anchor.Centre,
                     }
                 }
             });
